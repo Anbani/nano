b0f16d3a2dbd8348ddfbb940505c1f70a0665771
George Gach <georgegach@outlook.com>
2021-06-05 21:56:48 +0400

    init

---
+++ src/app.js
@@ -0,0 +1,46 @@
+String.prototype.toElement = function () {
+    var template = document.createElement("template");
+    var html = this.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+};
+
+function clickListener(s) {
+    let txt = document.querySelector(`#${s}Btn`).innerText
+    const body = document.querySelector('body');
+    const area = document.createElement('textarea');
+    body.appendChild(area);
+    area.value = txt;
+    area.select();
+    document.execCommand('copy');
+    body.removeChild(area);
+    console.log(txt, "copied")
+}
+
+function inputListener() {
+    results.innerHTML = "";
+    if (inputArea.value == '')
+        return
+    let scripts = [
+        "mkhedruli",
+        "mtavruli",
+        "asomtavruli",
+        "nuskhuri",
+        "phonetic",
+        "common",
+        "cyrillic",
+    ];
+    scripts.forEach((script) => {
+        results.appendChild(
+            `<button onclick="clickListener('${script}')" id="${script}Btn" class="result materialBtn">${anbani.core.interpret(
+                inputArea.value,
+                script
+            )}</button>`.toElement()
+        );
+    });
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    inputArea = document.querySelector("#input");
+    results = document.querySelector(".results");
+});
